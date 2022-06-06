19d896a831ac965be58abcbccc6ec8fbadbd2ab4
Carolina Hakamada <haka.carolina@gmail.com>
2022-06-06 19:42:42 -0300

    atualizando exercicios

---
+++ aula_09/dh-conta/src/main/java/com/resumo/dhconta/DhContaApplication.java
@@ -0,0 +1,13 @@
+package com.resumo.dhconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhContaApplication.class, args);
+	}
+
+}
---
+++ aula_09/dh-usuario/src/test/java/com/resumo/dhusuario/DhUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumo.dhusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
